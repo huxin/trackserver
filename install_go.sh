9136798ebe2aa48017f0776de413bc1718a96166
Xin Hu <huxin@pinterest.com>
2017-06-15 23:35:50 -0700

    build script

--- install_go.sh
+++ install_go.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 set -x
 set -e
 
@@ -6,6 +6,6 @@ cd /root
 wget "https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz" -O go.tar.gz
 tar zxvf go.tar.gz
 echo "export GOROOT=/root/go/" >> /root/.bashrc
-mkdir /root/proj
+mkdir -p /root/proj
 echo "export GOPATH=/root/proj" >> /root/.bashrc
 echo "export PATH=$PATH:$GOROOT/bin/" >> /root/.bashrc
